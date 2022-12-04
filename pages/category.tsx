745961d31d6a3a2de3b020bc0001e58e3befce59
lethanhan <lethanhancr7112001@gmail.com>
2022-12-04 16:54:52 +0700

    gdfgfd

--- pages/category.tsx
+++ pages/category.tsx
@@ -10,6 +10,7 @@ import "swiper/css";
 import useWidthSize from "../hooks/useWidthSize";
 import Sidebar from "../components/Sidebar/Sidebar";
 import { baseApi } from "../constant/constant";
+import MainLayout from "../components/LayOut/MainLayout";
 
 const Category: React.FC = () => {
   const [genres, setGenres] = useState<Genres[]>([]);
@@ -51,146 +52,150 @@ const Category: React.FC = () => {
 
   return (
     <>
-      <div className="text-center pb-4 h-auto">
-        {size.width && size.width < 1024 && (
-          <>
-            <div className="mt-12 px-6 ">
-              <Swiper
-                modules={[Navigation, Autoplay]}
-                grabCursor={true}
-                spaceBetween={0}
-                slidesPerView={10}
-                autoplay={{ delay: 2000 }}
-                navigation={false}
-                loop={true}
-                breakpoints={{
-                  375: {
-                    slidesPerView: 2,
-                    spaceBetween: 10,
-                  },
-                  414: {
-                    slidesPerView: 2,
-                    spaceBetween: 10,
-                  },
+      <MainLayout>
+        <div className="text-center pb-4 h-auto">
+          {size.width && size.width < 1024 && (
+            <>
+              <div className="mt-12 px-6 ">
+                <Swiper
+                  modules={[Navigation, Autoplay]}
+                  grabCursor={true}
+                  spaceBetween={0}
+                  slidesPerView={10}
+                  autoplay={{ delay: 2000 }}
+                  navigation={false}
+                  loop={true}
+                  breakpoints={{
+                    375: {
+                      slidesPerView: 2,
+                      spaceBetween: 10,
+                    },
+                    414: {
+                      slidesPerView: 2,
+                      spaceBetween: 10,
+                    },
 
-                  600: {
-                    slidesPerView: 3,
-                    spaceBetween: 10,
-                  },
-                  768: {
-                    slidesPerView: 4,
-                    spaceBetween: 10,
-                  },
-                  1024: {
-                    slidesPerView: 5,
-                    spaceBetween: 10,
-                  },
-                  1536: {
-                    slidesPerView: 10,
-                    spaceBetween: 10,
-                  },
-                }}
-              >
-                {genres.map((result: Genres) => (
-                  <>
-                    <SwiperSlide>
-                      {({ isActive }) => (
-                        <div>
-                          <button
-                            className={`${
-                              type === result.params
-                                ? "bg-slate-600 text-white"
-                                : ""
-                            } border-2 w-full py-2 `}
-                            onClick={() => setType(result.params)}
-                          >
-                            {result.name}
-                          </button>
-                        </div>
-                      )}
-                    </SwiperSlide>
-                  </>
-                ))}
-              </Swiper>
-            </div>
+                    600: {
+                      slidesPerView: 3,
+                      spaceBetween: 10,
+                    },
+                    768: {
+                      slidesPerView: 4,
+                      spaceBetween: 10,
+                    },
+                    1024: {
+                      slidesPerView: 5,
+                      spaceBetween: 10,
+                    },
+                    1536: {
+                      slidesPerView: 10,
+                      spaceBetween: 10,
+                    },
+                  }}
+                >
+                  {genres.map((result: Genres) => (
+                    <>
+                      <SwiperSlide>
+                        {({ isActive }) => (
+                          <div>
+                            <button
+                              className={`${
+                                type === result.params
+                                  ? "bg-slate-600 text-white"
+                                  : ""
+                              } border-2 w-full py-2 `}
+                              onClick={() => setType(result.params)}
+                            >
+                              {result.name}
+                            </button>
+                          </div>
+                        )}
+                      </SwiperSlide>
+                    </>
+                  ))}
+                </Swiper>
+              </div>
 
-            <div className="container w-full px-2 mt-12">
-              <div className="grid grid-cols-2 px-4 gap-x-4 gap-y-4 md:grid-cols-3 md:px-6 lg:px-0 lg:grid-cols-5 2xl:px-36 2xl:grid-cols-6 ">
-                {!loading ? (
-                  category.map((data) => (
-                    <ComicItem
-                      key={data.slug}
-                      name={data.name}
-                      image={data.image}
-                      url={data.url}
-                      slug={data.slug}
-                    />
-                  ))
-                ) : (
-                  <>
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                  </>
-                )}
+              <div className="container w-full px-2 mt-12">
+                <div className="grid grid-cols-2 px-4 gap-x-4 gap-y-4 md:grid-cols-3 md:px-6 lg:px-0 lg:grid-cols-5 2xl:px-36 2xl:grid-cols-6 ">
+                  {!loading ? (
+                    category.map((data) => (
+                      <ComicItem
+                        key={data.slug}
+                        name={data.name}
+                        image={data.image}
+                        url={data.url}
+                        slug={data.slug}
+                      />
+                    ))
+                  ) : (
+                    <>
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                    </>
+                  )}
+                </div>
+                <div className="pagination my-8">
+                  <Pagination
+                    setPage={setPage}
+                    page={page}
+                    countPages={countPages}
+                  />
+                </div>
               </div>
-              <div className="pagination my-8">
-                <Pagination
-                  setPage={setPage}
-                  page={page}
-                  countPages={countPages}
-                />
-              </div>
-            </div>
-          </>
-        )}
+            </>
+          )}
 
-        {size.width && size.width >= 1024 && (
-          <div className="flex flex-1 flex-wrap 2xl:px-36 lg:px-12">
-            <div className=" w-9/12 px-2 mt-8">
-              <p className="text-2xl font-semibold">Truyện thể loại: {type}</p>
-              <div className="grid grid-cols-2 px-4 gap-x-4 gap-y-4 md:grid-cols-3 md:px-6 lg:grid-cols-3 lg:px-8 2xl:px-12 2xl:grid-cols-4">
-                {!loading ? (
-                  category.map((data) => (
-                    <ComicItem
-                      key={data.slug}
-                      name={data.name}
-                      image={data.image}
-                      url={data.url}
-                      slug={data.slug}
-                    />
-                  ))
-                ) : (
-                  <>
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                    <SkeletonLoading />
-                  </>
-                )}
+          {size.width && size.width >= 1024 && (
+            <div className="flex flex-1 flex-wrap 2xl:px-36 lg:px-12">
+              <div className=" w-9/12 px-2 mt-6">
+                <p className="text-2xl font-semibold">
+                  Truyện thể loại: {type}
+                </p>
+                <div className="grid grid-cols-2 2xl:mt-6 px-4 gap-x-4 gap-y-4 md:grid-cols-3 md:px-6 lg:grid-cols-3 lg:px-8 2xl:px-12 2xl:grid-cols-4">
+                  {!loading ? (
+                    category.map((data) => (
+                      <ComicItem
+                        key={data.slug}
+                        name={data.name}
+                        image={data.image}
+                        url={data.url}
+                        slug={data.slug}
+                      />
+                    ))
+                  ) : (
+                    <>
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                      <SkeletonLoading />
+                    </>
+                  )}
+                </div>
+                <div className="pagination my-8">
+                  <Pagination
+                    setPage={setPage}
+                    page={page}
+                    countPages={countPages}
+                  />
+                </div>
               </div>
-              <div className="pagination my-8">
-                <Pagination
-                  setPage={setPage}
-                  page={page}
-                  countPages={countPages}
-                />
+              <div className=" w-3/12 mt-8 2xl:grid-cols-8 px-4">
+                <p className="py-2 bg-slate-700 text-white">
+                  <span>Thể Loại</span>
+                </p>
+                <Sidebar type={type} setType={setType} />
               </div>
             </div>
-            <div className=" w-3/12 mt-8 2xl:grid-cols-8 px-4">
-              <p className="py-2 bg-slate-700 text-white">
-                <span>Thể Loại</span>
-              </p>
-              <Sidebar type={type} setType={setType} />
-            </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </MainLayout>
     </>
   );
 };
