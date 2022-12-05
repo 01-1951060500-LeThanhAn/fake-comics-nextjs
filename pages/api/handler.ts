17c7e190855961e19581a0e7e15a4da74c192895
lethanhan <lethanhancr7112001@gmail.com>
2022-12-05 15:24:51 +0700

    fdsfsd

--- pages/api/handler.ts
+++ pages/api/handler.ts
@@ -1,9 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiHandler } from "next";
-import axios from "axios";
 
-var API_URL = `https://thanhan.vercel.app`;
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
